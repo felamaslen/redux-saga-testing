3df5e532e40b583c00d75ffe562ec9cd1aa45245
fela.maslen <fela.maslen@mubaloo.com>
2017-11-24 11:56:02 +0000

    Moved files to subdirectory

---
+++ app/index.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const express = require('express');
+const winston = require('winston');
+const { version } = require('./package.json');
+const evaluatePostfix = require('./evaluate-postfix');
+
+function run() {
+    const app = express();
+
+    app.get('/evaluate-postfix', (req, res) => {
+        const raw = decodeURIComponent(req.query.postfix || '');
+
+        try {
+            const result = evaluatePostfix(raw);
+
+            res.json({ result });
+        }
+        catch (err) {
+            res.status(400)
+                .json({ error: true });
+        }
+    });
+
+    app.set('view engine', 'ejs');
+    app.set('views', path.join(__dirname, 'src/templates'));
+
+    app.get('/', (req, res) => res.render('index', {
+        htmlWebpackPlugin: {
+            options: {
+                version,
+                externalStyles: process.env.NODE_ENV === 'production' ||
+                    (req.query.prod && req.query.prod === 'true')
+            }
+        }
+    }));
+
+    app.use(express.static(path.join(__dirname, 'static')));
+
+    const port = process.env.PORT || 3000;
+
+    app.listen(port, () => {
+        winston.log('info', `Server listening on port ${port}`);
+    });
+}
+
+module.exports = {
+    run
+};
+
---
+++ app/operators.js
@@ -0,0 +1,7 @@
+module.exports = {
+    '+': (arg1, arg2) => arg1 + arg2,
+    '-': (arg1, arg2) => arg1 - arg2,
+    '*': (arg1, arg2) => arg1 * arg2,
+    '/': (arg1, arg2) => arg1 / arg2
+};
+
