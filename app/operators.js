43e69ecdb0809fd9b1d67227acd1206bdd242dc9
fela.maslen <fela.maslen@mubaloo.com>
2017-12-01 11:19:28 +0000

    Handle power operator (^) on postfix evaluator

--- app/operators.js
+++ app/operators.js
@@ -2,14 +2,16 @@ const OPERATORS = {
     '*': (arg1, arg2) => arg1 * arg2,
     '/': (arg1, arg2) => arg1 / arg2,
     '+': (arg1, arg2) => arg1 + arg2,
-    '-': (arg1, arg2) => arg1 - arg2
+    '-': (arg1, arg2) => arg1 - arg2,
+    '^': (arg1, arg2) => arg1 ** arg2
 }
 
 const PRECEDENCE = {
-    '*': 0,
-    '/': 0,
-    '+': 1,
-    '-': 1
+    '*': 1,
+    '/': 1,
+    '+': 2,
+    '-': 2,
+    '^': 0
 };
 
 module.exports = { OPERATORS, PRECEDENCE };
